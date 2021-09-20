aa102f60b45ca7da074fb93db7061748ce43cd79
UglyClancy <duncan.brandx@gmail.com>
2021-09-20 13:59:08 -0700

    finished secondary functions, need work for connecting dictionary values

--- HikeRec.py
+++ HikeRec.py
@@ -13,7 +13,6 @@ def reccomendation_queue_direction():
     d_queue = Queue()
     print("Which area of Arizona would you like to hike today?")
     direction_input = input()
-    # if direction_input == "North":
     if direction_input in h_directions.keys():
         towns = h_directions[direction_input]
         print("Your hike choices include: ")
@@ -22,9 +21,40 @@ def reccomendation_queue_direction():
     else:
         print("Your choice must be North, South, East, or West.")
         return reccomendation_queue_direction()
-    return 
-   
 
-    
+t_queue = Queue()
+
+def rec_queue_town():
+    print("Which town would you like to hike near?")
+    town_input = input()
+    if town_input in h_locations.keys():
+        towns = h_locations[town_input]
+        print("Your hike choices include: ")
+        for hike in towns:
+            t_queue.enqueue(hike)
+    else:
+        print("Your choice must be one of the previously displayed locations.")
+        return rec_queue_town()
+
+def rec_queue_hike_diff():
+    print("What kind of hike are you looking for: easy, moderate, or hard?")
+    diff_input = input()
+    h_queue = Queue()
+    if not t_queue:
+        print("No hikes found in the are that are " + diff_input)
+    while t_queue:
+        if hikes.keys() in t_queue:
+            pass
+
+    # if diff_input not in hikes.values():
+    #     return True
+        # t_queue.pop()
+    # else:
+    #     return False
+        # print("Your choice must be one of the previously displayed locations.")
+        # return rec_queue_hike_diff()
+
 
 print(reccomendation_queue_direction())
+print(rec_queue_town())
+print(rec_queue_hike_diff())
